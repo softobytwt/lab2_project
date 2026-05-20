0c82b67ffbc355922499cac9b49fefec5d841493
softobytwt <sofabakarinova@gmail.com>
2026-05-20 23:30:50 +0300

    added function to haha_file that prints two seals

--- ARCHIVE_PYFILES/haha_file.py
+++ ARCHIVE_PYFILES/haha_file.py
@@ -2,4 +2,8 @@ def printseal():
     """prints a seal"""
     print("haha sea (:3 u )3")
     
-
+def printstwo():
+    """prints out TWO seals"""
+    print(" haha! i am seal one > (:3 u )3")
+    print(" (:3 u )3 < and im seal two!")
+    
